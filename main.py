e1e1865a985f0414f7c7ebd182e4ce6ba700d2df
Dokyun Kim <donny5557@gmail.com>
2023-06-25 11:32:04 -0400

    Build main page for bilboardle

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 """
 Website for playing Bilboardle
 """
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+from songs import get_bilboard
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html")
 
 @app.route('/bilboardle')
 def game():
-    pass
+    return render_template("game.html", songs = get_bilboard())
+  
 
 if __name__ == '__main__':
     app.run(debug=True)
